b04e8d4dcc55709b975575bfc39b2853da43a8bb
tasukaru <111254997+tasukaru@users.noreply.github.com>
2023-03-03 18:15:18 -0500

    testing

--- src/main/java/com/audieni/market/controllers/ProductController.java
+++ src/main/java/com/audieni/market/controllers/ProductController.java
@@ -5,11 +5,9 @@ import com.audieni.market.models.Product;
 import com.audieni.market.services.ProductService;
 import com.audieni.market.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,5 +24,17 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> findAllProducts() {
         return ResponseEntity.ok(productService.findAll());
+    }
+
+    @PostMapping("/addTestProducts")
+    public ResponseEntity<List<Product>> addProduct() {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product(1, "Product 1", 10));
+        productList.add(new Product(2, "Product 2", 20));
+        productList.add(new Product(3, "Product 3", 30));
+        productList.add(new Product(4, "Product 4", 40));
+        productList.add(new Product(5, "Product 5", 50));
+
+        return ResponseEntity.ok(productService.saveAll(productList));
     }
 }
